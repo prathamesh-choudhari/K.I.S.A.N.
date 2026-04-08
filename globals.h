bbc89c9946e8aaefb6adf612305a83d484db9d98
himanshu-garg00 <himanshugarg10923@gmail.com>
2026-04-08 13:48:38 +0530

    globals

---
+++ globals.h
@@ -0,0 +1,15 @@
+#ifndef globals
+#define globals
+
+#include "structs.h"
+
+extern soilData Soildata[100];
+extern int Soilcount;
+
+extern cost Costdata[20];
+extern int Costcount;
+
+extern char State[30][20];
+extern int Statecount;
+
+#endif
